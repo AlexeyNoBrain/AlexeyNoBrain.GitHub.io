71381b03a699f0b9838dc07fd1d4c65ff6888063
Ha1f15ch <alex.alexy98@mail.ru>
2020-01-21 20:19:04 +0300

    ттт

---
+++ form.php
@@ -0,0 +1,6 @@
+<form action="send.php" method="post">
+                        <input type="text" name="fio" placeholder="Укажите ФИО" required>
+                        <input type="text" name="email" placeholder="Укажите ваш e-mail" required>
+                        <input type="text" name="number" placeholder="Укажите ваш телефон" required>
+                        <input type="submit" value="Отправить">
+                    </form>
